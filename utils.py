3e427f3b6dd60fb45b9bf999998c746210995672
Cosmos <37209685+Cosmos01@users.noreply.github.com>
2025-09-07 01:32:09 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,19 @@
+import re
+from datetime import timedelta
+
+
+def get_time(kw):
+    match = re.search(r'(?P<num>[0-9]+)(?P<tp>小时|天|分|分钟)', kw)
+    if match and int(match.group('num')) > 0:
+        time = { '小时': 0, '分钟': 0, '天': 0 }
+        t = match.group('tp')
+        t = t if t != '分' else '分钟'
+        time[t] = int(match.group('num'))
+        return timedelta(days=time['天'], minutes=time['分钟'], hours=time['小时'])
+    else:
+        return timedelta(minutes=5)
+
+
 def get_time_text(seconds):
     if seconds > 3600:
         return f"{int(seconds / 3600)}小时{int(seconds % 3600 / 60)}分"
